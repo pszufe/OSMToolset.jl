2e6146b8e835da65810d6f197b5c57009ac543c7
JakubManikowski1 <jakub.manikowski@outlook.com>
2023-07-02 20:18:33 +0200

    New basic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,14 @@
 using OSMToolset
+using CSV, EzXML, DataFrames
+using Parsers
+import OpenStreetMapX: OSMData
 using Test
 
+pmap = joinpath(dirname(pathof(OSMToolset)),"..","test","data","map.osm")
+
+df = find_poi(pmap)
 @testset "OSMToolset" begin
 
-
+    @test nrow(df) == 909
 
 end
